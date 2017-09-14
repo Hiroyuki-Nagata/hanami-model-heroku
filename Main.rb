1c63e198faba2b25099da4d06dd7cad8afdeb636
Hiroyuki.Nagata <hiroyuki.nagata@veeva.com>
2017-09-14 17:02:47 +0900

    Add test files

---
+++ Main.rb
@@ -0,0 +1,24 @@
+require 'hanami/model'
+
+class User < Hanami::Entity
+end
+
+class UserRepository < Hanami::Repository
+end
+
+Hanami::Model.configure do
+  adapter :sql, ENV['DATABASE_URL']
+end.load!
+
+#repository = UserRepository.new
+#user       = repository.create(name: 'Luca')
+
+#puts user.id # => 1
+
+#found = repository.find(user.id)
+#found == user # => true
+
+#updated = repository.update(user.id, age: 34)
+#updated.age # => 34
+
+#repository.delete(user.id)
